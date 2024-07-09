ff7583885eb05add0eaf91571cbf36ae5c289874
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2024-07-09 11:51:58 -0700

    Movable crates

--- platformer-kit/entities/player/scripts/interaction_controller.gd
+++ platformer-kit/entities/player/scripts/interaction_controller.gd
@@ -1,36 +1,41 @@
 extends Node2D
 
-enum CollisionType {
-	PushableObject,
-	Damage,
-	NoOp
-}
-
 @onready var middle_right: RayCast2D = $MiddleRight
+@onready var middle_left: RayCast2D = $MiddleLeft
 @onready var middle_down: RayCast2D = $MiddleDown
 
 func _physics_process(delta: float) -> void:
-	var collision_type = handle_collisions(middle_right)
+	var right_interaction_event = handle_collisions(middle_right)
 
-	match collision_type:
-		CollisionType.PushableObject:
-			print("Need to push")
+	match right_interaction_event.get_interaction_type():
+		InteractionTypes.PushableObject:
+			var colliding_object = right_interaction_event.get_collider()
+			colliding_object.position.x += 100.0 * delta
+		_:
+			pass
+		
+	var left_interaction_event = handle_collisions(middle_left)
+
+	match left_interaction_event.get_interaction_type():
+		InteractionTypes.PushableObject:
+			var colliding_object = left_interaction_event.get_collider()
+			colliding_object.position.x -= 100.0 * delta
 		_:
 			pass
 		
 	var down_collision_type = handle_collisions(middle_down)
 
 	match down_collision_type:
-		CollisionType.Damage:
+		InteractionTypes.Damage:
 			print("Need to damage")
 		_:
 			pass
 
-func handle_collisions(ray: RayCast2D) -> CollisionType:
+func handle_collisions(ray: RayCast2D) -> InteractionEvent:
 	var collider = ray.get_collider()
 	if collider:
 		if collider is MovableCrate:
-			return CollisionType.PushableObject
+			return InteractionEvent.new(collider, InteractionTypes.PushableObject)
 		if collider is Spike:
-			return CollisionType.Damage
-	return CollisionType.NoOp
+			return InteractionEvent.new(collider, InteractionTypes.Damage)
+	return InteractionEvent.new(collider, InteractionTypes.NoOp)
---
+++ platformer-kit/entities/player/scripts/interaction_type.gd
@@ -0,0 +1,5 @@
+class_name InteractionTypes
+
+const PushableObject = "PushableObject"
+const Damage = "Damage"
+const NoOp = "NoOp"
