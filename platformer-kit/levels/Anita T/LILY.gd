cf7c76ddd979938eed902e3e6b8672c035634d0f
marijkekeyser <marijkekeyser@gmail.com>
2025-03-26 17:20:42 -0700

    Anita T

---
+++ platformer-kit/levels/Anita T/LILY.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 2000.0
+const JUMP_VELOCITY = - 2000.0
+
+var _spawn_position :=  Vector2()
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	_spawn_position = global_position
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	for idx in get_slide_collision_count():
+		var collision = get_slide_collision(idx)
+		if collision.get_collider().is_in_group("spike"):
+			global_position = _spawn_position
+	  
