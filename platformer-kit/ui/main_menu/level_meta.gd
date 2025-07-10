d006a9dfd230e8dc5eddb18b9bc7770fc5721c94
Diane Nguyen <me@dytrich_nguyen.com>
2025-07-10 13:20:32 -0700

    Add tooling to automate build processes

--- platformer-kit/ui/main_menu/level_meta.gd
+++ platformer-kit/ui/main_menu/level_meta.gd
@@ -4,6 +4,7 @@ class_name LevelMeta
 @export var level_name: String
 @export var author: String
 @export var file: String
+@export var scene_uid: String
 
 func _init(name: String = "", creator: String = "", path: String = ""):
 	level_name = name
