755da2548e6d0de10ddf0cb097a0ca594f352966
Diane Nguyen <me@dytrich_nguyen.com>
2025-07-10 14:39:35 -0700

    Expose move_speed variable

--- platformer-kit/entities/mushroom/scripts/state/move.gd
+++ platformer-kit/entities/mushroom/scripts/state/move.gd
@@ -1,7 +1,6 @@
 extends CharacterState
 
 var gravity = 980
-var move_speed = 100
 
 func enter():
 	animations.speed_scale = 2.0
@@ -15,7 +14,7 @@ func process_physics(delta: float) -> State:
 	if not parent.is_on_floor():
 		parent.velocity.y += gravity * delta
 	
-	var movement = movement_controller.get_vector() * move_speed
+	var movement = movement_controller.get_vector() * parent.move_speed
 	parent.velocity.x = movement.x
 	parent.move_and_slide()
 
