37c4eda510ad0e3b6ff90c184cf5ce76cc17ca38
Diane Nguyen <me@dytrich_nguyen.com>
2025-07-11 15:51:44 -0700

    hotfix for controllable platforms not resetting correctly

--- platformer-kit/entities/controllable_platform/controllable_platform.gd
+++ platformer-kit/entities/controllable_platform/controllable_platform.gd
@@ -1,10 +1,10 @@
 extends RigidBody2D
 
 var initial_position: Vector2
+var _reset = false
 
 func _ready():
 	freeze = true
-
 	initial_position = global_position
 	LevelController.register_entity_for_reset(self)
 
@@ -18,8 +18,17 @@ func unfreeze():
 	if freeze:
 		set_deferred("freeze", false)
 		# Hey why do I need this
-		global_position = initial_position
+		# global_position = initial_position
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if _reset:
+		linear_velocity = Vector2()
+		global_transform = Transform2D(0.0, initial_position)
+		_reset = false
+		set_deferred("freeze", true)
+	if freeze:
+		linear_velocity = Vector2()
+		global_transform = Transform2D(0.0, initial_position)
 
 func handle_reset():
-	freeze = true
-	linear_velocity = Vector2()
+	_reset = true
