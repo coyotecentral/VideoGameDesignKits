30b5cea0d6e94e36815a0ad0844a798ec774cd66
Diane Nguyen <me@dytrich_nguyen.com>
2025-07-24 16:55:11 -0700

    Checkpoint

--- platformer-kit/entities/player/scripts/state/dash_state.gd
+++ platformer-kit/entities/player/scripts/state/dash_state.gd
@@ -3,7 +3,7 @@ extends CharacterState
 @export_group("Transitions")
 @export var fall_state: State
 
-var dash_duration := 0.3
+var dash_duration := 0.2
 var upx_dash_dur := 0.3
 var updash_dur := 0.15
 
@@ -16,9 +16,18 @@ var up_dash_vel = 250.0 * 4.5
 var x_dash_vel = 250.0 * 7.0
 var upx_dash_vel = 250.0 * 4.5
 
-var upx_vel_falloff := 4.0
+var upx_vel_falloff := 3.5
+# var x_vel_falloff := 2.8
 var x_vel_falloff := 2.8
 var updash_velocity_falloff := 1.0
+
+var camera_shake := false
+var camera: Camera2D
+var init_camera_pos: Vector2
+var init_camera_position_smoothing: bool
+
+var shakes: int = 0
+var max_shakes: int = 6
 
 
 func enter():
@@ -34,11 +43,31 @@ func enter():
 	is_done = false
 	dash_start = true
 
+	camera = get_viewport().get_camera_2d()
+	if camera:
+		init_camera_position_smoothing = camera.position_smoothing_enabled
+		init_camera_pos = camera.position
+		if camera_shake:
+			camera.position_smoothing_enabled = false
+	shakes = 0
+
 func exit():
 	movement = Vector2.ZERO
 	is_done = false
+	if camera and camera_shake:
+		camera.position = init_camera_pos
+		camera.position_smoothing_enabled = init_camera_position_smoothing
 
 func process_physics(delta: float) -> State:
+	# Camera shake
+	if camera_shake:
+		if shakes < max_shakes:
+			var shake_magnitude = 2
+			camera.position = init_camera_pos + Vector2(randf() * shake_magnitude, randf() * shake_magnitude)
+			shakes += 1
+		else:
+			camera.position = init_camera_pos
+
 	# Dash exit
 	if movement.y == 0.0 and time >= dash_duration:
 		#horiz dash exit
@@ -66,20 +95,21 @@ func process_physics(delta: float) -> State:
 	else:
 		var sign = -1 if movement_controller.facing == "left" else 1
 		var init_x_vel = parent.velocity.x
-		var next_x_vel = init_x_vel * ease_out(min(time / upx_vel_falloff, 1.0))
+		var next_x_vel = init_x_vel * cube_ease_out(min(time / upx_vel_falloff, 1.0))
 
 		if movement.x != 0 && movement.y < 0:
 			# Updash out
-			parent.velocity.x *= ease_out(min(time / upx_vel_falloff, 1.0))
+			parent.velocity.x *= cube_ease_out(min(time / upx_vel_falloff, 1.0))
 		else:
-			parent.velocity.x *= ease_out(min(time / x_vel_falloff, 1.0))
+			# Horiz dash
+			parent.velocity.x *= cube_ease_out(min(time / x_vel_falloff, 1.0))
 
 		# falling
 		if movement.y < 0:
-			parent.velocity.y *= ease_out(min(time / updash_velocity_falloff, 1.0))
+			parent.velocity.y *= cube_ease_out(min(time / updash_velocity_falloff, 1.0))
 		elif movement.y > 0:
 			# down dash
-			parent.velocity.y *= ease_out(min(time / updash_velocity_falloff, 1.0))
+			parent.velocity.y *= cube_ease_out(min(time / updash_velocity_falloff, 1.0))
 		elif movement.x and time < 0.02:
 			parent.velocity.y = 0
 		else:
@@ -88,5 +118,11 @@ func process_physics(delta: float) -> State:
 	time += delta
 	return null
 
-func ease_out(x: float) -> float:
+func cube_ease_out(x: float) -> float:
 	return pow(1 - x, 3)
+
+func quad_ease_out(x: float) -> float:
+	return pow(1 - x, 2)
+
+func exp_ease_out(x: float) -> float:
+	return sqrt(1 - x)
